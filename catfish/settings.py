9d5d2da06240a550c59759d31816637747dc3bc8
touerangler <touer0018@gmail.com>
2023-11-21 23:24:39 +0800

    refactor: apart html css

--- catfish/settings.py
+++ catfish/settings.py
@@ -116,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -125,3 +128,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
--- catfish/views.py
+++ catfish/views.py
@@ -1,28 +1,25 @@
 import os
-import urllib.parse
 
 from django.core.cache import cache
 from django.http import FileResponse, HttpResponse
 from django.views import View
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.conf import settings
 from django.http import Http404
+from datetime import datetime
 
 
 class ListFilesView(View):
     @staticmethod
     def get(request):
         media_path = os.path.join(settings.BASE_DIR, 'media')
-        file_list = sorted(
-            [f for f in os.listdir(media_path) if os.path.isfile(os.path.join(media_path, f))],
-            key=lambda f: os.path.getmtime(os.path.join(media_path, f)),
-            reverse=True
-        )
-
-        print("File List:", file_list)  # 添加这一行来打印文件列表
-
-        return render(request, 'download.html', {'file_list': file_list})
-
+        files = [f for f in os.listdir(media_path) if os.path.isfile(os.path.join(media_path, f))]
+        sorted_files = sorted(files, key=lambda f: os.path.getmtime(os.path.join(media_path, f)), reverse=True)
+        file_dict: dict = {}
+        for file in sorted_files:
+            file_dict[file] = datetime.fromtimestamp(os.path.getmtime(os.path.join(media_path, file))).strftime(
+                "%Y-%m-%d %H:%M:%S")
+        return render(request, 'download.html', {'file_dict': file_dict})
 
 
 class DownloadFileView(View):
@@ -30,21 +27,17 @@ class DownloadFileView(View):
     def get(request, filename):
         file_path = os.path.join(settings.BASE_DIR, 'media', filename)
 
-        # Define a cache key for this user and file using IP address
         client_ip = request.META.get('REMOTE_ADDR')
         cache_key = f'download_limit:{client_ip}:{filename}'
 
-        # Get the current download count from cache, default to 0
         download_count = cache.get(cache_key, 0)
 
-        # Check if the user has exceeded the download limit
         if download_count >= 3:
-            ...
+            error_message = "You have reached the file download limit and are unable to download this file."
+            return HttpResponse(error_message, status=403)
 
-        # Increment the download count and update the cache
-        cache.set(cache_key, download_count + 1, 60)  # Cache expires in 60 seconds
+        cache.set(cache_key, download_count + 1, 60)
 
-        # Serve the file
         if os.path.exists(file_path):
             response = FileResponse(open(file_path, 'rb'))
             response['Content-Disposition'] = f'attachment; filename="{filename}"'
