576625f64fc09a9375546835849b02e97770ed55
Ensong Wei <61647484+touero@users.noreply.github.com>
2023-12-07 09:07:21 +0800

    Middleware check request's user_agent and time (#4)
    * docs: update README.md
    
    * feat: middleware check request's user_agent and time

---
+++ catfish/middleware.py
@@ -0,0 +1,26 @@
+import time
+from easierdocker import log
+from django.http import HttpResponseForbidden
+
+
+class CatfishMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.requests = {}
+
+    def __call__(self, request):
+        user_agent = request.META.get('HTTP_USER_AGENT', '').lower()
+        ip_address = request.META.get('REMOTE_ADDR')
+        log(f'[{ip_address}]: {user_agent}')
+
+        if 'mozilla' not in user_agent:
+            return HttpResponseForbidden("Forbidden: Bad User-Agent")
+        if ip_address in self.requests:
+            last_request_time = self.requests[ip_address]
+            elapsed_time = time.time() - last_request_time
+            if elapsed_time < 10:
+                return HttpResponseForbidden("Forbidden: Too many requests from this IP")
+        self.requests[ip_address] = time.time()
+
+        response = self.get_response(request)
+        return response
