9e98f98126384623c14061a3ca861a6e9e2e350f
Ensong Wei <61647484+touero@users.noreply.github.com>
2023-12-08 09:15:42 +0800

    Add middleware check request's user_agent and time (#5)
    * docs: update README.md
    
    * feat: middleware check request's user_agent and time
    
    * feat: update file

--- catfish/middleware.py
+++ catfish/middleware.py
@@ -12,13 +12,15 @@ class CatfishMiddleware:
         user_agent = request.META.get('HTTP_USER_AGENT', '').lower()
         ip_address = request.META.get('REMOTE_ADDR')
         log(f'[{ip_address}]: {user_agent}')
-
+        response = self.get_response(request)
+        if request.method != 'GET':
+            return response
         if 'mozilla' not in user_agent:
             return HttpResponseForbidden("Forbidden: Bad User-Agent")
         if ip_address in self.requests:
             last_request_time = self.requests[ip_address]
             elapsed_time = time.time() - last_request_time
-            if elapsed_time < 10:
+            if elapsed_time < 2:
                 return HttpResponseForbidden("Forbidden: Too many requests from this IP")
         self.requests[ip_address] = time.time()
 
