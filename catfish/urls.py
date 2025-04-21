4379d2999d3cf92ff36f985f8d2871cacc14197f
touero <touer0018@gmail.com>
2025-04-21 12:08:34 +0800

    feat: support custom file path
    refactor download file path to support custom file path
    add log

--- catfish/urls.py
+++ catfish/urls.py
@@ -5,6 +5,6 @@ from .views import ListFilesView, DownloadFileView, UploadFileView
 
 urlpatterns = [
     path('', ListFilesView.as_view(), name='list_files'),
-    path('download/<str:filename>/', DownloadFileView.as_view(), name='download_file'),
+    path('download/<path:filename>', DownloadFileView.as_view(), name='download_file'),
     path('upload/', UploadFileView.as_view(), name='upload_file'),
 ]
--- catfish/views.py
+++ catfish/views.py
@@ -1,9 +1,10 @@
-import os
+import logging
+import urllib.parse
 
-from easierdocker import log
-from django.core.cache import cache
+from pathlib import Path
+
 from django.core.files.storage import FileSystemStorage
-from django.http import FileResponse, HttpResponse, HttpResponseForbidden
+from django.http import FileResponse, HttpResponseForbidden
 from django.views import View
 from django.shortcuts import render, redirect
 from django.conf import settings
@@ -11,42 +12,46 @@ from django.http import Http404
 from datetime import datetime
 
 
+logger = logging.getLogger(__name__)
+
+
 class ListFilesView(View):
     @staticmethod
     def get(request):
-        media_path = os.path.join(settings.BASE_DIR, 'media')
-        files = [f for f in os.listdir(media_path) if os.path.isfile(os.path.join(media_path, f))]
-        sorted_files = sorted(files, key=lambda f: os.path.getmtime(os.path.join(media_path, f)), reverse=True)
-        file_dict: dict = {}
-        for file in sorted_files:
-            file_dict[file] = datetime.fromtimestamp(os.path.getmtime(os.path.join(media_path, file))).strftime(
-                "%Y-%m-%d %H:%M:%S")
-        return render(request, 'index.html', {'file_dict': file_dict})
+        show_dir = Path(settings.SHOW_DIR)
+        file_dict = {}
+
+        for path in show_dir.rglob('*'):
+            if path.is_file():
+                relative_path = path.relative_to(show_dir).as_posix()
+                mod_time = datetime.fromtimestamp(path.stat().st_mtime).strftime("%Y-%m-%d %H:%M:%S")
+                file_dict[relative_path] = mod_time
+
+        sorted_file_dict = dict(sorted(file_dict.items(), key=lambda item: (show_dir / item[0]).stat().st_mtime, reverse=True))
+
+        return render(request, 'index.html', {'file_dict': sorted_file_dict})
 
 
 class DownloadFileView(View):
     @staticmethod
     def get(request, filename):
-        file_path = os.path.join(settings.BASE_DIR, 'media', filename)
+        filename = filename.rstrip('/')
+        decoded_filename = urllib.parse.unquote(filename)
+        logger.info(f"will download file: {decoded_filename}")
 
-        client_ip = request.META.get('REMOTE_ADDR')
-        cache_key = f'download_limit:{client_ip}:{filename}'
+        safe_base = Path(settings.SHOW_DIR).resolve()
+        requested_path = (safe_base / decoded_filename).resolve()
 
-        download_count = cache.get(cache_key, 0)
+        if not str(requested_path).startswith(str(safe_base)):
+            raise Http404("Invalid file path.")
 
-        if download_count >= 3:
-            error_message = "You have reached the file download limit and are unable to download this file."
-            return HttpResponse(error_message, status=403)
-
-        cache.set(cache_key, download_count + 1, 60)
-
-        if os.path.exists(file_path):
-            response = FileResponse(open(file_path, 'rb'))
-            response['Content-Disposition'] = f'attachment; filename="{filename}"'
+        if requested_path.exists() and requested_path.is_file():
+            response = FileResponse(open(requested_path, 'rb'))
+            response['Content-Disposition'] = f'attachment; filename="{requested_path.name}"'
             response['Content-Type'] = 'application/octet-stream'
             return response
         else:
-            raise Http404
+            raise Http404("File not found.")
 
 
 class UploadFileView(View):
@@ -63,7 +68,7 @@ class UploadFileView(View):
         if request.FILES['file']:
             uploaded_file = request.FILES['file']
             fs = FileSystemStorage()
-            log(uploaded_file.name)
+            logger.info(uploaded_file.name)
             fs.save(uploaded_file.name, uploaded_file)
             return redirect('list_files')
 
