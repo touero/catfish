4d2c0dd9eff18c033547cd2ebc8f868260f10032
Ensong Wei <touer0018@gmail.com>
2025-04-30 17:12:13 +0800

    feat: support upload file and record in db (#16)
    * feat: turn to bootstrap card
    
    * fix: button and info is a line
    
    * feat: support custom file path
    
    refactor download file path to support custom file path
    add log
    
    * feat: support delete file
    
    * refactor: 重构文件列表视图并移除无关代码
    
    * feat: support user login
    
    Added a login page template, add user authentication and
    file deletion permission check, add user authentication
     and file deletion permission check
    
    * feat : user card
    
    * feat: support upload file
    
    * feat: support write to database to save info
    
    * fix: add icon for button
    
    * fix: update safe_base to use MEDIA_ROOT for file downloads

--- catfish/list_files_view.py
+++ catfish/list_files_view.py
@@ -1,34 +1,45 @@
 import logging
-
-from pathlib import Path
-
-from django.views import View
+from datetime import datetime
 from django.shortcuts import render
 from django.conf import settings
-from datetime import datetime
+from pathlib import Path
+from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.views import View
 from django.utils.decorators import method_decorator
 
-
+from .models import File
 
 logger = logging.getLogger(__name__)
-
-
 
 @method_decorator(login_required, name='dispatch')
 class ListFilesView(View):
     @staticmethod
     def get(request):
-        show_dir = Path(settings.SHOW_DIR)
+        show_dir = Path(settings.MEDIA_ROOT)
         file_dict = {}
-
 
         for path in show_dir.rglob('*'):
             if path.is_file():
                 relative_path = path.relative_to(show_dir).as_posix()
+                file_info = File.objects.filter(name=relative_path).first()
                 mod_time = datetime.fromtimestamp(path.stat().st_mtime).strftime("%Y-%m-%d %H:%M:%S")
-                file_dict[relative_path] = mod_time
+
+                if file_info:
+                    local_time = timezone.localtime(file_info.uploaded_at)
+                    file_dict[relative_path] = {
+                        'description': file_info.description,
+                        'uploaded_by': file_info.uploaded_by,
+                        'uploaded_at': local_time.strftime("%Y-%m-%d %H:%M:%S"),
+                    }
+                else:
+                    file_dict[relative_path] = {
+                        'description': "description is null",
+                        'uploaded_by': "MEDIA_ROOT dir",
+                        'uploaded_at': mod_time,
+                    }
 
         sorted_file_dict = dict(sorted(file_dict.items(), key=lambda item: (show_dir / item[0]).stat().st_mtime, reverse=True))
+
         can_delete = request.user.has_perm("catfish.can_delete_file")
         return render(request, 'index.html', {'file_dict': sorted_file_dict, 'can_delete': can_delete})
--- catfish/models.py
+++ catfish/models.py
@@ -1,9 +1,12 @@
+from datetime import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
 class File(models.Model):
     name = models.CharField(max_length=255)
-    uploaded_by = models.CharField(max_length=100)
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
 
     class Meta:
         permissions = [
@@ -12,3 +15,6 @@ class File(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_uploaded_at_in_shanghai(self):
+        return timezone.localtime(self.uploaded_at)
